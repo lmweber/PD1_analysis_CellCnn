8f5f96bdf60aed1dcaf1fdb0e86331b9b6ebe9b9
Lukas Weber <lukmweber@gmail.com>
2017-04-03 15:27:22 +0200

    add heatmap of median marker expression

--- results_CellCnn_base_combined.R
+++ results_CellCnn_base_combined.R
@@ -6,11 +6,15 @@
 ###########################################################################
 
 
-# note: run after script 'run_CellCnn_PD1_base_combined.R', to get 'samples' and
-# 'condition' vectors
+# note: run previous script 'run_CellCnn_PD1_base_combined.R' to get the following
+# vectors: 'samples', 'condition', 'markers_ix'
 
 
 library(dplyr)
+library(flowCore)
+library(pheatmap)
+library(RColorBrewer)
+
 
 
 # -------------------------------------------
@@ -58,7 +62,8 @@ res %>%
   summarize(mean(freq_pct), 
             sd(freq_pct), 
             sem(freq_pct), 
-            median(freq_pct)) -> 
+            median(freq_pct), 
+            mad(freq_pct)) -> 
   res_cond
 
 res_cond
@@ -70,7 +75,94 @@ res
 sink()
 
 sink("../results/summary_statistics_selected_filter_by_group.txt")
-res_cond
+t(res_cond)
 sink()
+
+
+
+# -------------------------------------------------------------------------
+# heatmap of marker expression for selected filter/population vs. all cells
+# -------------------------------------------------------------------------
+
+# note: load marker column indices ('markers_ix') from previous spreadsheet: 'run_CellCnn_PD1_base_combined.R'
+
+
+# indices of cells in selected filter/population
+
+ix_sel <- lapply(res_sel, function(r) {
+  which(r[, 2] == 1)
+})
+
+ix_sel
+
+
+# load transformed data files and subset cells
+
+files_transf <- list.files("../data_transformed", full.names = TRUE)
+
+data <- lapply(files_transf, function(f) {
+  exprs(read.FCS(f, transformation = FALSE, truncate_max_range = FALSE))
+})
+
+data_sel <- mapply(function(f, ix) {
+  d <- exprs(read.FCS(f, transformation = FALSE, truncate_max_range = FALSE))
+  d_sel <- d[ix, , drop = FALSE]
+}, files_transf, ix_sel)
+
+
+# marker columns only
+
+data_markers <- lapply(data, function(d) {
+  d[, markers_ix, drop = FALSE]
+})
+
+data_sel_markers <- lapply(data_sel, function(d) {
+  d[, markers_ix, drop = FALSE]
+})
+
+# check
+t(sapply(data_markers, colnames))
+t(sapply(data_sel_markers, colnames))
+
+
+# calculate median marker expression by sample
+
+meds <- t(sapply(data_markers, function(d) {
+  apply(d, 2, median)
+}))
+
+meds_sel <- t(sapply(data_sel_markers, function(d) {
+  apply(d, 2, median)
+}))
+
+rownames(meds) <- samples
+rownames(meds_sel) <- samples
+
+meds
+meds_sel
+
+
+# calculate overall medians (median of sample medians; i.e. we are weighting samples equally)
+
+meds_group <- apply(meds, 2, median)
+meds_sel_group <- apply(meds_sel, 2, median)
+
+meds_group
+meds_sel_group
+
+
+# heatmap
+
+meds_plot <- rbind(meds_group, meds_sel_group)
+rownames(meds_plot) <- c("all cells", "selected population")
+
+pheatmap(meds_plot, 
+         color = colorRampPalette(brewer.pal(7, "YlGnBu"))(100), 
+         cluster_rows = FALSE, cluster_cols = FALSE, 
+         display_numbers = TRUE, number_color = "black", fontsize_number = 9, 
+         main = "Median (transformed) marker expression: all cells vs. selected population", 
+         filename = "../plots/heatmap_marker_exp.pdf", 
+         width = 12, height = 2.5)
+
 
 
