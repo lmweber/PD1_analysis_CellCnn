1a6802ebe1423f6319aa1d90316ba105f48837d1
Lukas Weber <lukmweber@gmail.com>
2017-04-04 13:44:32 +0200

    add statistical tests (mixed models)

--- results_CellCnn_base_combined.R
+++ results_CellCnn_base_combined.R
@@ -1,9 +1,9 @@
-###########################################################################
-# Script to calculate summary statistics for results from previous script #
-# ("run_CellCnn_PD1_base_combined.R")                                     #
-#                                                                         #
-# Lukas Weber, March 2017                                                 #
-###########################################################################
+#######################################################################################
+# Script to calculate summary statistics and heatmap for results from previous script #
+# ("run_CellCnn_PD1_base_combined.R")                                                 #
+#                                                                                     #
+# Lukas Weber, March 2017                                                             #
+#######################################################################################
 
 
 # note: run previous script 'run_CellCnn_PD1_base_combined.R' to get the following
@@ -12,6 +12,8 @@
 
 library(dplyr)
 library(flowCore)
+library(lme4)
+library(multcomp)
 library(pheatmap)
 library(RColorBrewer)
 
@@ -38,17 +40,18 @@ res_sel <- lapply(files_sel, read.csv)
 n_cells <- sapply(res_sel, nrow)
 n_cells
 
-freq <- sapply(res_sel, function(d) sum(d[, 2]))
-freq
+n_selected <- sapply(res_sel, function(d) sum(d[, 2]))
+n_selected
 
 
 # create data frame of results
 res <- data.frame(
-  sample = samples, 
+  sample = factor(samples), 
   condition = factor(condition), 
   n_cells = n_cells, 
-  n_selected = freq, 
-  freq_pct = round(freq / n_cells * 100, 2)
+  n_selected = n_selected, 
+  proportion = n_selected / n_cells, 
+  prop_pct = round(n_selected / n_cells * 100, 2)
 )
 
 res
@@ -59,11 +62,11 @@ sem <- function(x) sd(x) / sqrt(length(x))
 
 res %>% 
   group_by(condition) %>% 
-  summarize(mean(freq_pct), 
-            sd(freq_pct), 
-            sem(freq_pct), 
-            median(freq_pct), 
-            mad(freq_pct)) -> 
+  summarize(mean(prop_pct), 
+            sd(prop_pct), 
+            sem(prop_pct), 
+            median(prop_pct), 
+            mad(prop_pct)) -> 
   res_cond
 
 res_cond
@@ -80,15 +83,46 @@ sink()
 
 
 
+# ---------------------------------------------------------------
+# statistical tests: using generalized linear mixed models (GLMM)
+# ---------------------------------------------------------------
+
+# define model formula
+formula_glmer <- proportion ~ condition + (1|sample)
+
+
+# fit model
+fit <- glmer(formula_glmer, weights = n_cells, family = binomial, data = res)
+
+fit
+summary(fit)
+
+
+# hypothesis test and p-value
+contrast <- as.matrix(t(data.frame(conditionR = c(0, 1))))
+contrast
+
+hyp <- glht(fit, linfct = contrast)
+
+hyp
+summary(hyp)
+
+p_val <- summary(hyp)$test$pvalues
+p_val
+
+
+# save results
+sink("../results/mixed_model_test_results.txt")
+summary(hyp)
+sink()
+
+
+
 # -------------------------------------------------------------------------
 # heatmap of marker expression for selected filter/population vs. all cells
 # -------------------------------------------------------------------------
 
-# note: load marker column indices ('markers_ix') from previous spreadsheet: 'run_CellCnn_PD1_base_combined.R'
-
-
 # indices of cells in selected filter/population
-
 ix_sel <- lapply(res_sel, function(r) {
   which(r[, 2] == 1)
 })
@@ -97,7 +131,6 @@ ix_sel
 
 
 # load transformed data files and subset cells
-
 files_transf <- list.files("../data_transformed", full.names = TRUE)
 
 data <- lapply(files_transf, function(f) {
@@ -111,7 +144,6 @@ data_sel <- mapply(function(f, ix) {
 
 
 # marker columns only
-
 data_markers <- lapply(data, function(d) {
   d[, markers_ix, drop = FALSE]
 })
@@ -126,7 +158,6 @@ t(sapply(data_sel_markers, colnames))
 
 
 # calculate median marker expression (pooled cells)
-
 meds <- apply(do.call(rbind, data_markers), 2, median)
 meds_sel <- apply(do.call(rbind, data_sel_markers), 2, median)
 
@@ -135,7 +166,6 @@ meds_sel
 
 
 # # alternatively: calculate median marker expression by sample (i.e. samples equally weighted)
-# 
 # meds <- t(sapply(data_markers, function(d) {
 #   apply(d, 2, median)
 # }))
@@ -152,7 +182,6 @@ meds_sel
 # 
 # 
 # # calculate overall medians (median of sample medians; i.e. we are weighting samples equally)
-# 
 # meds_group <- apply(meds, 2, median)
 # meds_sel_group <- apply(meds_sel, 2, median)
 # 
@@ -160,18 +189,17 @@ meds_sel
 # meds_sel_group
 
 
-# heatmap
-
 # meds_plot <- rbind(meds_group, meds_sel_group)
 # rownames(meds_plot) <- c("all cells", "selected population")
 
+
+# create heatmap
 meds_plot <- rbind(meds, meds_sel)
 rownames(meds_plot) <- c("all cells", "selected")
 
 pheatmap(meds_plot, 
          color = colorRampPalette(brewer.pal(7, "YlGnBu"))(100), 
          cluster_rows = FALSE, cluster_cols = FALSE, 
-         display_numbers = TRUE, number_color = "black", fontsize_number = 9, 
          main = "Median (transformed) marker expression: all cells vs. selected population", 
          filename = "../plots/heatmap_marker_exp.pdf", 
          width = 12, height = 2.5)
